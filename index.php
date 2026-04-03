011efea9ed8d0ad43f53279f0ee150e09290af82
salsabila <salsabiladiva96898@gmail.com>
2026-04-03 20:25:44 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'transferBank.php';
+require_once 'e-wallet.php';
+require_once 'qris.php';
+
+// objek
+$transfer = new TransferBank(100000);
+$ewallet = new Ewallet(50000);
+$qris = new QRIS(75000);
+
+// output
+echo $transfer->prosesPembayaran();
+echo "<br>";
+echo $transfer->cetakStruk();
+
+echo "<hr>";
+
+echo $ewallet->prosesPembayaran();
+echo "<br>";
+echo $ewallet->cetakStruk();
+
+echo "<hr>";
+
+echo $qris->prosesPembayaran();
+echo "<br>";
+echo $qris->cetakStruk();
+?>
